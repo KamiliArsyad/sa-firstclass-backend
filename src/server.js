d447d9b99cd0c48d3b7c7ea03061b678dfb62f30
KamiliArsyad <arsyad.ik@gmail.com>
2023-06-24 20:44:56 +0800

    Setup db

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const connectDB = require("./config/dbConfig");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
